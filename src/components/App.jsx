8e278262b5778500e00a546add0d428a8dcc43e5
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-09-08 12:02:56 +0200

    fix state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -20,14 +15,15 @@ export class App extends Component {
       ...data,
       id: nanoid(),
     };
+
     this.setState(prev => ({
       contacts: [contact, ...prev.contacts],
     }));
+
     this.state.contacts.map(
       el =>
         el.name === data.name && alert(data.name + ' is already in contacts.')
     );
-    // el.name === data.name && alert(data.name + ' is already in contacts.')
   };
 
   deleteUser = id => {
